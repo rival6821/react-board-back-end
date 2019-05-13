68d8969bdcc644037ac53d583f8a34e5028a884d
ilhoon <rival6821@naver.com>
2019-05-13 23:07:34 +0900

    회원가입 프론트 완성

--- routes/user.js
+++ routes/user.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const { User } = require('../models');
+const bcrypt = require('bcrypt');
 
-// 회원가입시 중복체크
+// 회원가입시 중복체크, 이메일, 닉네임
 router.post('/dupTest', async (req, res, next) => {
 	const { type, data } = req.body;
 	try {
@@ -18,6 +19,24 @@ router.post('/dupTest', async (req, res, next) => {
 			res.send('exist');
 		}
 	} catch (error) {
+		console.error(error);
+		next(error);
+	}
+});
+
+// 회원가입 처리
+router.post('/join', async (req, res, next) => {
+	const { email, nickname, password } = req.body;
+	try {
+		const hash = await bcrypt.hash(password, 12);
+		await User.create({
+			email: email,
+			nickname: nickname,
+			password: hash,
+		});
+		res.send('ok');
+	} catch (error) {
+		console.error(error);
 		next(error);
 	}
 });
